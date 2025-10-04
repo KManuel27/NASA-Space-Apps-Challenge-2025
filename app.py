5eae53c105a9d390eef380aa2bc7f362665fdb0e
Ayash Rath <ayashrath@gmail.com>
2025-10-04 15:14:37 +0100

    PEP Spacing

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 def start_page():
     return render_template("startPage.html")  # No Python computation yet
 
+
 @app.route("/available_meteors")
 def available_meteors():
     # show page with date pickers and optionally fetch data
@@ -32,12 +33,6 @@ def available_meteors():
     # render the template and inject initial data for the JS
     return render_template("availableMeteors.html", start_date=start, end_date=end, asteroids=asteroids, error=error)
 
-# --------------- Page 2: Meteor Visualization ---------------
-@app.route("/meteors")
-def meteor_list():  # ORBITAL MECHANICS AND VISUALISATION CODE:
-    # Legacy route (not used directly). Redirect to visualization UI.
-    return redirect(url_for('start_page'))
-
 
 @app.route('/meteors/visualize/<asteroid_id>')
 def visualize_asteroid(asteroid_id: str):
@@ -55,6 +50,7 @@ def visualize_asteroid(asteroid_id: str):
         graph_html = meteor_viz.simulate_sun_earth_asteroid()
 
     return render_template('meteorViz.html', graph_html=graph_html)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
